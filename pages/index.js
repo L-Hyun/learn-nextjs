4868766f38db222b11cef964225e42f0349a35db
K <dkh1001@outlook.kr>
2022-05-31 21:38:40 +0900

    Make List, Item

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
+import axios from "axios";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import ItemList from "../components/ItemList";
 
 export default function Home() {
+	const [list, setList] = useState([]);
+	const API_URL =
+		"http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+
+	function getData() {
+		axios.get(API_URL).then((res) => {
+			setList(res.data);
+		});
+	}
+
+	useEffect(() => {
+		getData();
+	}, []);
+
 	return (
 		<>
 			<Head>
-				<title>Learn-Next.js</title>
+				<title>Learn-Next.js | L-Hyun</title>
 			</Head>
-			<div>index</div>
+			<div>Index</div>
+			<ItemList list={list} />
 		</>
 	);
 }
+
+/*
+_app.js나 각 페이지에서 Head 태그를 통해서 html문서의 head부분 수정 가능
+*/
--- pages/views/[id].js
+++ pages/views/[id].js
@@ -1,3 +1,34 @@
-export default function ID() {
-	return <div>ID</div>;
-}
+import Axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Item } from "semantic-ui-react";
+
+const Post = () => {
+	const router = useRouter();
+	const { id } = router.query;
+
+	const [item, setItem] = useState({});
+
+	const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+
+	function getData() {
+		Axios.get(API_URL).then((res) => {
+			setItem(res.data);
+		});
+	}
+
+	useEffect(() => {
+		if (id && id > 0) {
+			getData();
+		}
+	}, [id]);
+
+	return <Item item={item} />;
+};
+
+export default Post;
+
+/*
+Dynamic Route
+- [].js로 라우팅 받을 변수 저장
+ */
